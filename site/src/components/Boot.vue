dab14f989cf20d7f524c7d5a89169345c41acd27
Asad <asadkhan91003@gmail.com>
2025-09-05 02:00:07 +0100

    Minor changes to boot banner

--- site/src/components/Boot.vue
+++ site/src/components/Boot.vue
@@ -7,10 +7,11 @@
 ██╔═══╝ ██║   ██║██╔══██╗   ██║   ██╔══╝  ██║   ██║██║     ██║██║   ██║
 ██║     ╚██████╔╝██║  ██║   ██║   ██║     ╚██████╔╝███████╗██║╚██████╔╝
 ╚═╝      ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝      ╚═════╝ ╚══════╝╚═╝ ╚═════╝
-Welcome to my linux terminal style portfolio :) - Asad Ali Khan.
-History search, more files, tabbing files and mobile compatibility in the works! 🏗️
-Non-technical? No problem! Try typing "help" to get you started.
-Still stuck? Try out  "whoami", "cat about.txt" and "cat projects.txt"
+👋 Welcome to my linux terminal style portfolio :) - Asad Ali Khan. 👋
+🏗️ History search, more files, tabbing files and mobile compatibility in the works! 🏗️
+
+⁉️ Non-technical? No problem! Try typing "help" to get you started.
+Still stuck? Try out  "whoami", "cat about.txt" and "cat projects.txt". ⁉️
 </pre>
   </div>
 </template>
